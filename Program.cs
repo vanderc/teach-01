4e3df17c9a7b33dce740ec07ce4c18b40f377ccc
vanderc <167822234+vanderc@users.noreply.github.com>
2024-04-27 09:37:32 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * CSE212 
+ * (c) BYU-Idaho
+ * 01-Teach
+ * 
+ * It is a violation of BYU-Idaho Honor Code to post or share this code with others or 
+ * to post it online.  Storage into a personal and private repository (e.g. private
+ * GitHub repository, unshared Google Drive folder) is acceptable.
+ */
+
+using teach_01;
+
+Console.WriteLine("\n======================\nDivisors\n======================");
+Divisors.Run();
+
+Console.WriteLine("\n======================\nArray Selector\n======================");
+ArraySelector.Run();
+
+// Uncomment and run as you get to the solution part
+// Console.WriteLine("\n======================\nDivisors Solution\n======================");
+// DivisorsSolution.Run();
+//
+// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+// ArraySelectorSolution.Run();
